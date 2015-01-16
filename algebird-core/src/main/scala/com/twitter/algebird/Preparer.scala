d33be076f76d9b5999be93e0237a1e3bc71dccf7
Avi Bryant <avi@stripe.com>
2015-01-15 21:17:53 -0800

    f-bounded poly

--- algebird-core/src/main/scala/com/twitter/algebird/Preparer.scala
+++ algebird-core/src/main/scala/com/twitter/algebird/Preparer.scala
@@ -1,37 +1,56 @@
 package com.twitter.algebird
 
-case class Preparer[A, T](prepareFn: A => T) {
+trait MonoidAggregatable[A,T] {
+  def monoidAggregate[B, C](aggregator: MonoidAggregator[T, B, C]): Aggregator[A, B, C]
+
+  def count(pred: T => Boolean) = monoidAggregate(Aggregator.count(pred))
+}
+
+trait Aggregatable[A,T] extends MonoidAggregatable[A,T] {
+  def aggregate[B, C](aggregator: Aggregator[T, B, C]): Aggregator[A, B, C]
+  def monoidAggregate[B, C](aggregator: MonoidAggregator[T, B, C]) = aggregate(aggregator)
+}
+
+trait Preparer[A, T, +This[A, T] <: Preparer[A, T, This]] extends MonoidAggregatable[A,T] {
+  def map[U](fn: T => U): This[A, U]
+
+  def flatMap[U](fn: T => TraversableOnce[U]): FlatPreparer[A, U]
+
+  def lift[B, C](aggregator: Aggregator[T, B, C]): MonoidAggregator[A, Option[B], Option[C]] =
+    monoidAggregate(aggregator.lift)
+
+  def split[B1, B2, C1, C2](fn: This[A, T] => (Aggregator[A, B1, C1], Aggregator[A, B2, C2])): Aggregator[A, (B1, B2), (C1, C2)] = {
+    val (a1, a2) = fn(this.asInstanceOf[This[A, T]])
+    a1.join(a2)
+  }
+}
+
+object Preparer {
+  def apply[A]: SimplePreparer[A, A] = SimplePreparer[A, A](identity)
+}
+
+case class SimplePreparer[A, T](prepareFn: A => T)
+  extends Preparer[A, T, SimplePreparer]
+  with Aggregatable[A, T] {
+
   def map[U](fn: T => U) =
-    Preparer[A, U](fn.compose(prepareFn))
+    SimplePreparer[A, U](fn.compose(prepareFn))
 
   def flatMap[U](fn: T => TraversableOnce[U]) =
     FlatPreparer[A, U](fn.compose(prepareFn))
 
   def aggregate[B, C](aggregator: Aggregator[T, B, C]): Aggregator[A, B, C] =
     aggregator.composePrepare(prepareFn)
-
-  def split[B1, B2, C1, C2](fn: Preparer[A, T] => (Aggregator[A, B1, C1], Aggregator[A, B2, C2])): Aggregator[A, (B1, B2), (C1, C2)] = {
-    val (a1, a2) = fn(this)
-    a1.join(a2)
-  }
 }
 
-object Preparer {
-  def apply[A]: Preparer[A, A] = Preparer[A, A](identity)
-}
-
-case class FlatPreparer[A, T](prepareFn: A => TraversableOnce[T]) {
-  def map[U](fn: T => U): FlatPreparer[A, U] =
+case class FlatPreparer[A, T](prepareFn: A => TraversableOnce[T])
+  extends Preparer[A, T, FlatPreparer] {
+  def map[U](fn: T => U) =
     FlatPreparer { a: A => prepareFn(a).map(fn) }
 
-  def flatMap[U](fn: T => TraversableOnce[U]): FlatPreparer[A, U] =
+  def flatMap[U](fn: T => TraversableOnce[U]) =
     FlatPreparer { a: A => prepareFn(a).flatMap(fn) }
 
-  def aggregate[B, C](aggregator: MonoidAggregator[T, B, C]): MonoidAggregator[A, B, C] =
+  def monoidAggregate[B, C](aggregator: MonoidAggregator[T, B, C]): MonoidAggregator[A, B, C] =
     aggregator.sumBefore.composePrepare(prepareFn)
-
-  def split[B1, B2, C1, C2](fn: FlatPreparer[A, T] => (Aggregator[A, B1, C1], Aggregator[A, B2, C2])): Aggregator[A, (B1, B2), (C1, C2)] = {
-    val (a1, a2) = fn(this)
-    a1.join(a2)
-  }
 }
